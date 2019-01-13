da54ffebdb338cd7039c0ef1cba00fd6ed741714
Lareta <lareta064@gmail.com>
2019-01-13 12:49:23 +0300

    компонент Слайдер

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,6 +3,7 @@ import React, { Component } from 'react';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import styled from 'styled-components';
+import Slider from './Slider';
 
 const Repair = styled.div`
 	color:#fff;
@@ -55,14 +56,13 @@ class Main extends React.Component {
 					<CallButton>Отправить заявку!</CallButton>
 
 				</Col>
-				<Col lg={6}>
-
+				<Col lg={6} lgOffset={1}>
+					<Slider/>
 				</Col>
 
 			</Row>
 
 			)
-
 	}
 }
 export default Main
